8bfe538fb7a8473a38989bfc86be252e771e6c9e
Saiesh Kamble <64722232+WodleHudga@users.noreply.github.com>
2023-09-04 22:14:41 +0530

    Add files via upload

---
+++ UserDefinedExceptionHandling/QuickKart/QuickKartBL/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        private double price;
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidPriceException("\nPrice cannot be less than or equal to zero. " +
+                      "\nProduct details cannot be stored.\n");
+                }
+                else
+                {
+                     this.price = value;
+                }
+            }
+        }
+
+        public int QuantityAvailable { get; set; }
+
+        public Product(string productId, string productName, double price, int quantityAvailable)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            QuantityAvailable = quantityAvailable;
+        }
+    }
+}
---
+++ UserDefinedExceptionHandling/QuickKart/QuickKartBL/InvalidTravelClassException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class InvalidPriceException : Exception
+    {
+        public InvalidPriceException()
+        {
+        }
+        public InvalidPriceException(string message) : base(message)
+        {
+        }
+    }
+
+
+}
